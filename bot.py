b8405ac51d5e29ec08e3ccd6672bf90476c01666
sofiagadea <slgadea@miuandes.cl>
2022-11-07 07:43:23 -0300

    message handler

--- bot.py
+++ bot.py
@@ -3,18 +3,33 @@ from flask import Flask, request,jsonify
 import json
 
 
-TOKEN = '5718500704:AAGcjqRiyZ0DpzJXa25X_FcY03Xzzg6aPFo'
+TOKEN = '5607369368:AAHEBTuVYxEGuyyBp2gZeP5vqOcMyhkkt4M'
 
 app = Flask(__name__)
 
 
+def welcome_message(item):
+    print(item)
+    if item['text'].lower() == 'hi':
+        msg = 'hello'
+        chat_id = item['chat']['id']
+        user_id = item['from']['id']
+        username = item['from']['username']
+        welcome_msg = f'{msg}{username}'
+        to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={welcome_msg}&parse_mode=HTML'
+        resp = requests.get(to_url)
 
 @app.route("/",methods= ["GET",'POST'])
 def hello_world():
     if request.method == 'POST':
         data = request.get_json()
         print(f'DATA: {data}')
-        return{'statusCode': 200, 'body': 'Success','data': data}
+        if 'message'in data:
+            data = data['message']
+            welcome_message(data)
+            return{'statusCode': 200, 'body': 'Success','data': data}
+        else:
+            return{'statusCode': 404, 'body': 'User has left the chat room and deleted the chat','data': data}
     else:
         return{'statusCode':200, 'body':'Success'}
 
