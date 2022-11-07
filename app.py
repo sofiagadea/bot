7aca43e4325d64a2435406ea722a38fdf17a5d88
sofiagadea <slgadea@miuandes.cl>
2022-11-07 09:27:07 -0300

    arreglo2

--- app.py
+++ app.py
@@ -7,11 +7,18 @@ TOKEN = '5607369368:AAHEBTuVYxEGuyyBp2gZeP5vqOcMyhkkt4M'
 
 app = Flask(__name__)
 
+'''{'message_id': 14, 
+'from': {'id': 5667263033, 'is_bot': False, 'first_name': 'Sofia', 'last_name': 'Gadea', 'username': 'sofigadea', 'language_code': 'es'}, 
+ 'chat': {'id': 5667263033, 'first_name': 'Sofia', 'last_name': 'Gadea', 'username': 'sofigadea', 
+ 'type': 'private'}, 
+ 'date': 1667819834, 'text': 'hi'}'''
+
 
 def welcome_message(item):
     print(item)
-    if item['text'].lower() == 'hi':
-        msg = 'hello'
+    
+    if item.get('text') == 'hi':
+        msg = 'hello '
         chat_id = item['chat']['id']
         user_id = item['from']['id']
         username = item['from']['username']
