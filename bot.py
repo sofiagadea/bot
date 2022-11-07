0b8f6dba82385cb3aca02ba1d17dde48ea91d062
sofiagadea <slgadea@miuandes.cl>
2022-11-07 08:08:05 -0300

    requirements

--- bot.py
+++ bot.py
@@ -4,34 +4,18 @@ import json
 
 
 TOKEN = '5607369368:AAHEBTuVYxEGuyyBp2gZeP5vqOcMyhkkt4M'
-
+LINK = 'https://api.telegram.org/bot5607369368:AAHEBTuVYxEGuyyBp2gZeP5vqOcMyhkkt4M/setWebhook?url=https://com-bot'
 app = Flask(__name__)
 
 
-def welcome_message(item):
-    print(item)
-    if item['text'].lower() == 'hi':
-        msg = 'hello'
-        chat_id = item['chat']['id']
-        user_id = item['from']['id']
-        username = item['from']['username']
-        welcome_msg = f'{msg}{username}'
-        to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={welcome_msg}&parse_mode=HTML'
-        resp = requests.get(to_url)
-
-@app.route("/",methods= ["GET",'POST'])
+@app.route("/", methods=['GET','POST'])
 def hello_world():
     if request.method == 'POST':
         data = request.get_json()
         print(f'DATA: {data}')
-        if 'message'in data:
-            data = data['message']
-            welcome_message(data)
-            return{'statusCode': 200, 'body': 'Success','data': data}
-        else:
-            return{'statusCode': 404, 'body': 'User has left the chat room and deleted the chat','data': data}
+        return {'statusCode': 200, 'body': 'Success', 'data': data}
     else:
-        return{'statusCode':200, 'body':'Success'}
+        return {'statusCode': 200, 'body': 'Success'}
 
 if __name__ == '__main__':
     app.run(debug=True)
