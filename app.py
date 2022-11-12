a2fd93a4414a94675dc651035659ace1d8613560
sofiagadea <slgadea@miuandes.cl>
2022-11-12 18:06:43 -0300

    6

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import requests
 from flask import Flask, request,jsonify
 import json
+import random
 
 globvar = 0
 TOKEN = '5529622158:AAHYUHjS8xoigtNWrXLGLxzTc5M6TSzGA18'
 globallist = []
 ids = []
+intro = 0
+game = 0
 """https://api.telegram.org/bot5529622158:AAHYUHjS8xoigtNWrXLGLxzTc5M6TSzGA18/setWebhook?url=https://bot-wcsg.herokuapp.com/"""
+
 
 app = Flask(__name__)
 '''{'message_id': 14, 
@@ -30,6 +34,7 @@ class ListUsers():
         return s
     """
 
+
 class Player():
     def __init__(self,id,username,points,tries_left):
         self.id = id
@@ -38,15 +43,30 @@ class Player():
         self.tries_left = tries_left
 
 class Number():
-    def __init__(self,status, number, maximum,minimum,winner,tries):
+    def __init__(self):
         self.status = 0
-        self.number = 10
-        self.maximum = maximum
-        self.minomum = minimum
-        self.winner = winner
-        self.tries = tries
+        self.number = 0
+        self.maximum = 0
+        self.tries = 5
+        self.advanced = 0
+        self.qm = 0
+        self.qt = 0
+
+    def playing(self):
+        self.status = 1
+
+    def started(self):
+        self.advanced = 1
+    
+    def number_guess(self,number):
+        num = random.randint(0,self.maximum)
+        self.number = num
 
 
+
+
+
+number = Number()
 
 """def set_global_to_one():
     global globvar 
@@ -58,6 +78,9 @@ def add_player(objeto):
     globallist.append(objeto)
     ids.append(objeto.id)
 
+def stop_intro():
+    global intro
+    intro = 1
 
 
 def print_list():
@@ -70,47 +93,95 @@ def print_list():
 def welcome_message(item):
     print(item)
     global globvar
-
+    global game
     if 'text' in item:
         chat_id = item['chat']['id']
+        global intro
 
-        if item['text'] == "nuevo usuario":
-            user_id = item['from']['id']
-            if 'username' in item['from']:
-                username = item['from']['username']
-                add_player(Player(user_id ,username,0,100))
+        if intro == 0:       
+            if item['text'] == "nuevo usuario":
+                global ids
+                if item['from']['id'] not in ids:
+                    user_id = item['from']['id']
+                    if 'username' in item['from']:
+                        username = item['from']['username']
+                        add_player(Player(user_id ,username,0,100))
+                    
+                    else:
+                        username = item['from']['first_name'] 
+                        add_player(Player(user_id ,username,0,100))
+                    msg = f'Bienvenido {username}'
+                    
+                    to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                    resp = requests.get(to_url)
+
+                else:
+                    msg = f'Usuario ya existe'
+                    
+                    to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                    resp = requests.get(to_url)                    
                 
+
+                msg = 'Seleccionar un juego'+"\n"+ "1 --> Number"+ "\n" + "2 --> Trivia"
+                to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                resp = requests.get(to_url)
+
+
+            elif item['text'] == "1":
+                stop_intro()
+                global number
+                number.started()
+                msg = 'Inicio del juego Number' + "\n" + "Jugadores: " + "\n" + print_list()
+        
+                to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                resp = requests.get(to_url)      
+                game = "number"
+                msg = "Diga un máximo"
+            
+                to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                resp = requests.get(to_url)      
+                game = "number"
+                
+
             else:
-                username = item['from']['first_name'] 
-                add_player(Player(user_id ,username,0,100))
-                
-            msg = f'Bienvenido {username}'
-            
-            to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
-            resp = requests.get(to_url)
-            msg = 'Seleccionar un juego'+"\n"+ "1 --> Number"+ "\n" + "2 --> Trivia"
-            to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
-            resp = requests.get(to_url)
-
-        elif item['text'] == "1":
-            
-            msg = 'Inicio del juego Number' + "\n" + "Jugadores: " + "\n" + print_list()
-       
-            to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
-            resp = requests.get(to_url)      
+                print("ID PERSONA MENSAJE: ",item['from']['id'])
+                global ids
+                print("TABLA IDS", ids)
+                if item['from']['id'] not in ids:   
+                    msg = 'Escribir "Nuevo usuario" para ser agregado al juego'
+                    to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                    resp = requests.get(to_url)
 
         else:
-            print("ID PERSONA MENSAJE: ",item['from']['id'])
-            global ids
-            print("TABLA IDS", ids)
-            if item['from']['id'] not in ids:   
-                msg = 'Escribir "Nuevo usuario" para ser agregado al juego'
-                to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
-                resp = requests.get(to_url)
-            else:
-                msg = 'Poner "LISTO" para empezar' 
-                to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
-                resp = requests.get(to_url)
+            if game == "number":
+                if number.maximum == 0:
+                    if item['text'].isnumeric():
+                        number.maximum = int(item['text'])
+                        number.number_guess()
+                    else:
+                        msg = 'El texto debe ser un número'
+                        to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                        resp = requests.get(to_url) 
+
+                elif number.qt == 0:
+                        msg = "Diga el número de intentos por jugador"
+                        to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                        resp = requests.get(to_url)  
+                        number.qt = 1
+                elif number.tries == 0:
+                    if item['text'].isnumeric():
+                        number.tries = int(item['text'])
+                    else:
+                        msg = 'El texto debe ser un número'
+                        to_url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={msg}&parse_mode=HTML'
+                        resp = requests.get(to_url)                       
+
+               
+
+
+                
+
+
 
     else:
         print("No hay text")
